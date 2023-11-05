c78f45d7b650c6f7925bbc625dbb4f9a3b6b60fc
shovan <karnashovan2@email.com>
2023-11-05 16:53:44 +0530

    Added, reviews, trailer features and Completed the HEro Compoenent

--- src/App.js
+++ src/App.js
@@ -4,20 +4,41 @@ import api from './api/axiosConfig.js';
 import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
 
-  const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [allMovies, setAllMovies] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getAllMovies = async () => {
-
     try {
       const response = await api.get("/api/v1/movies/getallmovies")
-      setMovies(response.data);
+      setAllMovies(response.data);
       console.log(response.data);
     } catch (error) {
       console.log("ERROR in getMovies: ", error);
     }
+  }
+
+  const getMovieDataById = async (movieId) => {
+
+    try {
+      const response = await api.get(`/api/v1/movies/getmovie/${movieId}`);
+
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      console.log(singleMovie);
+      setReviews(singleMovie.reviewIds);
+    }
+    catch (error) {
+      console.error(error);
+    }
+
   }
 
   useEffect(() => {
@@ -27,15 +48,16 @@ function App() {
 
   return (
     <div className="App">
-
+      <Header />
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path='/' element={<Home movies={movies} />}>
-
-          </Route>
+          <Route path="/" element={<Home movies={allMovies} />} ></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieDataById} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Route>
       </Routes>
-    </div>
+    </div >
   );
 }
 
